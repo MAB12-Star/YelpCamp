7d4ca729e75fed232d41a2d6be6a35a292c9c834
Michael <michael.aron.blue@gmail.com>
2023-11-21 15:39:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -92,6 +92,7 @@ const store = MongoStore.create({
     }
 });
 
+
 // const store = new MongoDBStore({
 //     url: mongodb://127.0.0.1:27017/yelp-camp' || "mongodb+srv://Admin01:aKUp1aVGsYuAebAs@cluster0.omts09l.mongodb.net/?retryWrites=true&w=majority",
 //     secret: 'testing',
