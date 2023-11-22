6b7abb382bc026e56bf818e590d1bb1b2c740692
Michael <michael.aron.blue@gmail.com>
2023-11-22 11:32:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -70,6 +70,8 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
+// Enable profiling for all queries that take longer than 100 milliseconds
+db.setProfilingLevel(1, { slowms: 100 });
 
 const app = express();
 
